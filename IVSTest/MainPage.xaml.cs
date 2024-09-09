74dcc66cbdd3503a253e2083b3de55f339db5b62
あんのたん® <dev@annotunzdy.info>
2024-09-10 03:39:41 +0900

    Add Example Project

---
+++ IVSTest/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace IVSTest
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            MyLabel1.Text = "辻";
+            MyLabel2.Text = "辻" + char.ConvertFromUtf32(0xE0103);
+            MyLabel3.Text = "辻";
+            MyLabel4.Text = "辻" + char.ConvertFromUtf32(0xE0100);
+        }
+    }
+
+}
---
+++ IVSTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IVSTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
